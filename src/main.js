b5bf9ea40445a020ae025557d4889f4022b39638
Maksym Dzyubenko <ckraer@gmail.com>
2025-02-06 00:32:50 +0200

    Make slider from projects list using Swiper

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Swiper from "swiper";
-/*
+import { Navigation } from "swiper/modules";
 import 'swiper/css';
+
+Swiper.use([Navigation]);
 const projectsSwiper = new Swiper(".projects-swiper", {
     direction: "horizontal",
-    loop: false,
     navigation: {
         nextEl: ".projects-swiper-button-next",
         prevEl: ".projects-swiper-button-prev"
     },
-    spaceBetween: 70
+    spaceBetween: 100
 });
-*/
