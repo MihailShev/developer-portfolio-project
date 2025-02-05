24e9e5fcbf76b08a270c51400d61fe12f51cc491
Maksym Dzyubenko <ckraer@gmail.com>
2025-02-05 23:31:10 +0200

    Add styles to entire section

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Swiper from "swiper";
+/*
+import 'swiper/css';
+const projectsSwiper = new Swiper(".projects-swiper", {
+    direction: "horizontal",
+    loop: false,
+    navigation: {
+        nextEl: ".projects-swiper-button-next",
+        prevEl: ".projects-swiper-button-prev"
+    },
+    spaceBetween: 70
+});
+*/
