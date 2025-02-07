e3e17fb10dce4d4c853913e80c7fcf54ec508cb7
Maxim Dzyubenko <ckraer@gmail.com>
2025-02-07 14:33:49 +0200

    Remove default styles and fix main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,1 @@
-import Swiper from "swiper";
-import { Navigation } from "swiper/modules";
-import 'swiper/css';
-
-Swiper.use([Navigation]);
-const projectsSwiper = new Swiper(".projects-swiper", {
-    direction: "horizontal",
-    navigation: {
-        nextEl: ".projects-swiper-button-next",
-        prevEl: ".projects-swiper-button-prev"
-    },
-    spaceBetween: 100
-});
+import "./js/projects.js";
