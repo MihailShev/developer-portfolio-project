a4062b867077d8310913501d6f60b8578a33cf99
Halyna Myronova <galinka091189@gmail.com>
2025-02-08 09:33:19 +0100

    add fag

---
+++ src/js/fag.js
@@ -0,0 +1,25 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+new Accordion('.accordion-container');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const triggers = document.querySelectorAll('.ac-trigger');
+
+  triggers.forEach(trigger => {
+    trigger.addEventListener('click', function () {
+      const parent = this.closest('.list-item');
+      const iconUp = this.querySelector('.icon-up');
+      const iconDown = this.querySelector('.icon-down');
+
+      // Переключаем класс "hidden" на иконках
+      if (parent.classList.contains('is-active')) {
+        iconUp.classList.remove('hidden');
+        iconDown.classList.add('hidden');
+      } else {
+        iconUp.classList.add('hidden');
+        iconDown.classList.remove('hidden');
+      }
+    });
+  });
+});
