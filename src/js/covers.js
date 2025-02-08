a3ac60201b9a92984c63416fe9e90e74f11b6844
SignEd1366 <vovichcoolguy@gmail.com>
2025-02-08 15:44:40 +0200

    correct errors 2

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,8 +1,13 @@
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return rect.top < window.innerHeight && rect.bottom > 0;
+}
+
 function handleScroll() {
 const firstRowItems = document.querySelectorAll('.first-row-item');
 const secondRowItems = document.querySelectorAll('.second-row-item');
 
-let inViewport = true;
+let inViewport = false;
 
 firstRowItems.forEach(item => {
 if (isInViewport(item)) {
@@ -12,9 +17,14 @@ inViewport = true;
 
 if (inViewport) {
 firstRowItems.forEach(item => item.classList.add('animate'));
-secondRowItems.forEach(item => item.classList.add('animate'));
+secondRowItems.forEach(item => item.classList.add('animateReverse'));
 } else {
 firstRowItems.forEach(item => item.classList.remove('animate'));
-secondRowItems.forEach(item => item.classList.remove('animate'));
+secondRowItems.forEach(item => item.classList.remove('animateReverse'));
+    console.log("Hello!");
 }
 }
+
+window.addEventListener('scroll', handleScroll);
+
+window.addEventListener('load', handleScroll);
