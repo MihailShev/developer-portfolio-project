331853983be1f2592f8c9144727d1e6e5d48c718
Art-Rud <poltavarud@gmail.com>
2025-02-09 16:38:01 +0200

    corrected + svg + scroll + loader

--- src/js/reviews-rendering.js
+++ src/js/reviews-rendering.js
@@ -7,20 +7,28 @@ export const createRewiewsCard = cardInfo => {
 };
 export const createErrorCard = () => {
   return `<li class="reviews-item swiper-slide error-item">
-    <img class="reviews-img" src="#" alt="Error">
-    <h3 class="reviews-name error-title">Name : not found</h3>
+    <svg class="reviews-img reviews-img-err" width="48" height="48">
+          <use href="../img/icons/icons.svg#img-err"></use>
+        </svg>
+    <h3 class="reviews-name error-title">Name not found</h3>
     <p class="reviews-text error-text">Not found</p>
-</li><li class="reviews-item swiper-slide error-item tablet-err">
-    <img class="reviews-img" src="#" alt="Error">
-    <h3 class="reviews-name error-title">Name : not found</h3>
+</li><li class="reviews-item swiper-slide error-item tablet">
+    <svg class="reviews-img reviews-img-err" width="48" height="48">
+          <use href="../img/icons/icons.svg#img-err"></use>
+        </svg>
+    <h3 class="reviews-name error-title">Name not found</h3>
     <p class="reviews-text error-text">Not found</p>
-</li><li class="reviews-item swiper-slide error-item desctop-err">
-    <img class="reviews-img" src="#" alt="Error">
-    <h3 class="reviews-name error-title">Name : not found</h3>
+</li><li class="reviews-item swiper-slide error-item desctop">
+    <svg class="reviews-img reviews-img-err" width="48" height="48">
+          <use href="../img/icons/icons.svg#img-err"></use>
+        </svg>
+    <h3 class="reviews-name error-title">Name not found</h3>
     <p class="reviews-text error-text">Not found</p>
-</li><li class="reviews-item swiper-slide error-item desctop-err">
-    <img class="reviews-img" src="#" alt="Error">
-    <h3 class="reviews-name error-title">Name : not found</h3>
+</li><li class="reviews-item swiper-slide error-item desctop">
+    <svg class="reviews-img reviews-img-err" width="48" height="48">
+          <use href="../img/icons/icons.svg#img-err"></use>
+        </svg>
+    <h3 class="reviews-name error-title">Name not found</h3>
     <p class="reviews-text error-text">Not found</p>
 </li>`;
 };
--- src/js/reviews.js
+++ src/js/reviews.js
@@ -2,13 +2,18 @@ import { fetchReviews } from './reviews-request';
 import { createRewiewsCard } from './reviews-rendering';
 import { createErrorCard } from './reviews-rendering';
 import Swiper from 'swiper/bundle';
-
 const reviewsList = document.querySelector('.reviews-list');
+const revBtn = document.querySelectorAll('.rev-btn');
+const loader = document.querySelector('.loader-wrapper');
+revBtn.forEach(btn => {
+  btn.classList.add('disable-btn');
+});
 const createRewiews = async () => {
   try {
     const { data } = await fetchReviews();
     const cardTemplate = data.map(el => createRewiewsCard(el)).join('');
     reviewsList.innerHTML = cardTemplate;
+    reviewsList.style.gap = 0;
     const swiper = new Swiper('.reviews-wrapper', {
       initialSlide: 0,
       slidesPerView: 'auto',
@@ -39,6 +44,30 @@ const createRewiews = async () => {
     });
   } catch (error) {
     reviewsList.innerHTML = createErrorCard();
+    revBtn.forEach(btn => {
+      btn.classList.add('swiper-button-disabled');
+    });
+  } finally {
+    setTimeout(() => {
+      revBtn.forEach(btn => {
+        btn.classList.remove('disable-btn');
+      });
+      loader.style.opacity = 0;
+      loader.classList.add('visually-hidden');
+    }, 1500);
+    setTimeout(() => {
+      loader.classList.remove('visible');
+    }, 2000);
   }
 };
-createRewiews();
+
+const isReview = () => {
+  const section = document.querySelector('.reviews');
+  const secTop = section.getBoundingClientRect().top;
+  const secHeight = window.innerHeight;
+  if (secTop < secHeight) {
+    createRewiews();
+    window.removeEventListener('scroll', isReview);
+  }
+};
+window.addEventListener('scroll', isReview);
