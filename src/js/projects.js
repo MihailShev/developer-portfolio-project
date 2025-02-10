ee7a83a79072736b0ee6038a0cf5163ac6def19c
Maksym Dzyubenko <ckraer@gmail.com>
2025-02-10 18:21:15 +0200

    Make projects swiper movable by keyboard (in viewport)

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,13 +1,17 @@
 import Swiper from "swiper";
-import { Navigation } from "swiper/modules";
+import { Navigation, Keyboard } from "swiper/modules";
 import 'swiper/css';
 
-Swiper.use([Navigation]);
+Swiper.use([Navigation, Keyboard]);
 const projectsSwiper = new Swiper(".projects-swiper", {
     direction: "horizontal",
     navigation: {
         nextEl: ".projects-swiper-button-next",
         prevEl: ".projects-swiper-button-prev"
+    },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true
     },
     spaceBetween: 100
 });
