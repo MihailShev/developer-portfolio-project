8dc027d05edfe4002aa6d9d2913e01f121a84f77
Mihail <mykh.shev4enko@gmail.com>
2025-02-10 15:26:49 +0200

    Done

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -5,7 +5,7 @@ export const createModalWT = () => {
   <div class="modal-wt">
   <button class="modal-wt__btn" type="button">
     <svg class="modal-wt__icon" width="12" height="12" aria-label="close modal window">
-      <use href="../img/icons/icons.svg#icon-close"></use>
+      <use href="./img/icons/icons.svg#icon-close"></use>
     </svg>
   </button>
   <h3 class="modal-wt__title">Thank you for your interest in cooperation!</h3>
