516ffead47711e3a771c0e0c24dade3d89c89984
Mihail <mykh.shev4enko@gmail.com>
2025-02-11 21:15:09 +0200

    small-fix

--- src/js/fag.js
+++ src/js/fag.js
@@ -2,6 +2,7 @@ import Accordion from 'accordion-js';
 
 const accordionFaq = new Accordion('.accordion-container', {
   duration: 900,
+  showMultiple: true,
   elementClass: 'list-item',
   triggerClass: 'btn-faq',
   panelClass: 'list-item-text',
