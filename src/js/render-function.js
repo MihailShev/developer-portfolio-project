cfa520f9f112713cb44cc9b570bfdef73e3c6675
Mihail <mykh.shev4enko@gmail.com>
2025-02-11 23:02:51 +0200

    fix icon-close footer-modal

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -4,9 +4,7 @@ export const createModalWT = () => {
   modalElWT.innerHTML = `
   <div class="modal-wt">
   <button class="modal-wt__btn" type="button">
-    <svg class="modal-wt__icon" width="12" height="12" aria-label="close modal window">
-      <use href="/img/icons/icons.svg#icon-close"></use>
-    </svg>
+
   </button>
   <h3 class="modal-wt__title">Thank you for your interest in cooperation!</h3>
   <p class="modal-wt__text">The manager will contact you shortly to discuss further details and opportunities for
