dd6d03f37f7b06b3e049ecb8295e15d5130907b3
Mihail <mykh.shev4enko@gmail.com>
2025-02-13 13:53:20 +0200

    fix sw,js

--- src/js/chash-site.js
+++ src/js/chash-site.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('../sw.js')
+    .register('/sw.js')
     .then(reg => console.log('✅ Service Worker:', reg))
     .catch(err => console.error('❌  Service Worker:', err));
 }
